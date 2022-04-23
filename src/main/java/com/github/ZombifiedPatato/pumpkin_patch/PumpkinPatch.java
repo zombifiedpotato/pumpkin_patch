21c2c4fda3435fe7304e525235ef646c89d53746
zombified_patato <bryanbelle2003@gmail.com>
2022-04-23 21:50:56 +0200

    Added a comment as a reminder

--- src/main/java/com/github/ZombifiedPatato/pumpkin_patch/PumpkinPatch.java
+++ src/main/java/com/github/ZombifiedPatato/pumpkin_patch/PumpkinPatch.java
@@ -9,7 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PumpkinPatch implements ModInitializer {
-
+	//gamerule dolimitedcrafting for blocking crafting recipes
 	public static final String MOD_ID = "pumpkin_patch";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
