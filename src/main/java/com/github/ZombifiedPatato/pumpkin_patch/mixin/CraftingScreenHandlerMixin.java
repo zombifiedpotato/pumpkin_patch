d1fe5ff7451e4f2460336e1066fcf9421c42722c
zombified_patato <bryanbelle2003@gmail.com>
2022-04-25 22:20:11 +0200

    Finished mixin for crafting recipe blocking and added 3 test origins, 1 for every mod.

--- src/main/java/com/github/ZombifiedPatato/pumpkin_patch/mixin/CraftingScreenHandlerMixin.java
+++ src/main/java/com/github/ZombifiedPatato/pumpkin_patch/mixin/CraftingScreenHandlerMixin.java
@@ -1,16 +1,57 @@
 package com.github.ZombifiedPatato.pumpkin_patch.mixin;
 
+import com.github.ZombifiedPatato.pumpkin_patch.PumpkinPatch;
+import io.github.apace100.origins.origin.Origin;
+import io.github.apace100.origins.origin.OriginLayer;
+import io.github.apace100.origins.origin.OriginLayers;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.CraftingInventory;
+import net.minecraft.inventory.CraftingResultInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.network.packet.s2c.play.ScreenHandlerSlotUpdateS2CPacket;
+import net.minecraft.recipe.CraftingRecipe;
+import net.minecraft.recipe.RecipeType;
 import net.minecraft.screen.CraftingScreenHandler;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.World;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.*;
 
 @Mixin(CraftingScreenHandler.class)
 public class CraftingScreenHandlerMixin {
 
-	@ModifyVariable(method = "updateResult", at = @At("STORE"), ordinal = 0)
-	private static ItemStack craftedItem(ItemStack itemStack) {
-		return ItemStack.EMPTY;
+	@Inject(method = "updateResult", at = @At(value = "TAIL", shift = At.Shift.BY, by = -3), cancellable = true)
+	private static void craftedItem(ScreenHandler handler, World world, PlayerEntity player, CraftingInventory craftingInventory, CraftingResultInventory resultInventory, CallbackInfo callbackInfo) {
+		List<String> mi_origins = List.of("test_1");
+		List<String> botania_origins = List.of("test_2");
+		List<String> bewitchment_origins = List.of("test_3");
+		try {
+			Optional<CraftingRecipe> optional = world.getServer().getRecipeManager().getFirstMatch(RecipeType.CRAFTING, craftingInventory, world);
+			if (optional.isPresent()){
+				String namespace = optional.get().getId().getNamespace();
+				HashMap<OriginLayer,Origin> origins = Origin.get(player);
+				String origin = origins.get(OriginLayers.getLayer(new Identifier(PumpkinPatch.MOD_ID, "races"))).getIdentifier().getPath();
+				if (
+						(namespace.equals("modern_industrialization") && !mi_origins.contains(origin)) ||
+						(namespace.equals("botania") && !botania_origins.contains(origin)) ||
+						(namespace.equals("bewitchment") && !bewitchment_origins.contains(origin))
+				){
+					ItemStack itemStack = ItemStack.EMPTY;
+					ServerPlayerEntity serverPlayerEntity = (ServerPlayerEntity) player;
+					resultInventory.setStack(0, itemStack);
+					handler.setPreviousTrackedSlot(0, itemStack);
+					serverPlayerEntity.networkHandler.sendPacket(new ScreenHandlerSlotUpdateS2CPacket(handler.syncId, handler.nextRevision(), 0, itemStack));
+					callbackInfo.cancel();
+				}
+			}
+		}catch (Exception ignored) {}
 	}
 }
