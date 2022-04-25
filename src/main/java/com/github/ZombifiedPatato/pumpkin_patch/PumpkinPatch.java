bbbc8c4dae10e3da3d37ff1b682f2854be725aa3
zombified_patato <bryanbelle2003@gmail.com>
2022-04-25 22:21:08 +0200

    Removed comment reminder.

--- src/main/java/com/github/ZombifiedPatato/pumpkin_patch/PumpkinPatch.java
+++ src/main/java/com/github/ZombifiedPatato/pumpkin_patch/PumpkinPatch.java
@@ -10,9 +10,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PumpkinPatch implements ModInitializer {
-	//gamerule dolimitedcrafting for blocking crafting recipes
-	//RecipeUnlocker mixin -> check for recipe.getId().getNamespace().equals("modern_industrialization"); and origin
-	//net/minecraft/recipe/RecipeUnlocker.java
 	public static final String MOD_ID = "pumpkin_patch";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
