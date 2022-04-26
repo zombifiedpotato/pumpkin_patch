3e0e86fcfba367213243ff198a82e1d536dc03b9
zombified_patato <bryanbelle2003@gmail.com>
2022-04-26 16:09:19 +0200

    Added explorer dimension (no reset) with portal (block and ignite will be changed later)

--- src/main/java/com/github/ZombifiedPatato/pumpkin_patch/PumpkinPatch.java
+++ src/main/java/com/github/ZombifiedPatato/pumpkin_patch/PumpkinPatch.java
@@ -5,9 +5,14 @@ import com.github.ZombifiedPatato.pumpkin_patch.entity.ModEntities;
 import com.github.ZombifiedPatato.pumpkin_patch.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 import com.github.ZombifiedPatato.pumpkin_patch.particle.ModParticles;
+import net.kyrptonaught.customportalapi.api.CustomPortalBuilder;
+import net.minecraft.block.Blocks;
 import net.minecraft.recipe.Recipe;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.awt.*;
 
 public class PumpkinPatch implements ModInitializer {
 	public static final String MOD_ID = "pumpkin_patch";
@@ -19,5 +24,15 @@ public class PumpkinPatch implements ModInitializer {
 		ModBlocks.registerBlocks();
 		ModEntities.registerModEntities();
 		ModParticles.registerModParticles();
+		registerPortals();
+	}
+
+	private void registerPortals() {
+		CustomPortalBuilder.beginPortal()
+				.frameBlock(Blocks.AMETHYST_BLOCK)
+				.destDimID(new Identifier("pumpkin_patch", "explorer_world"))
+				.tintColor(255, 246, 168)
+				.registerPortal();
+
 	}
 }
